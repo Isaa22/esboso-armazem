41c21f2c9fbe508ef0cd9df40734166ec045ca8e
Maria Isabely <m.carvalho25@escola.pr.gov.br>
2025-04-28 00:11:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,17 @@ let carrinho = [];
 let total = 0;
 
 function adicionarAoCarrinho(nomeProduto, precoProduto) {
-    
     carrinho.push({ nome: nomeProduto, preco: precoProduto });
-
-   
     atualizarCarrinho();
-
-    
     total += precoProduto;
     document.getElementById("total").innerText = total.toFixed(2);
-
-   
     atualizarItensCarrinho();
 }
 
 function atualizarCarrinho() {
     const cartCount = document.getElementById("cartCount");
     cartCount.innerText = carrinho.length;
-    
-  
+
     const carrinhoElement = document.getElementById("carrinho");
     if (carrinho.length > 0) {
         carrinhoElement.classList.remove("carrinho-fechado");
@@ -60,9 +52,11 @@ function finalizarPedido() {
     }
     const pedido = carrinho.map(item => `${item.nome} - R$${item.preco.toFixed(2)}`).join('\n');
     const mensagem = `Olá, gostaria de fazer um pedido:\n\n${pedido}\n\nTotal: R$${total.toFixed(2)}`;
-
     const whatsappUrl = `https://wa.me/5543998306254?text=${encodeURIComponent(mensagem)}`;
     window.open(whatsappUrl, "_blank");
+
+    // Gerar o QR Code de pagamento
+    gerarQRCode(pagamentoLink);
 }
 
 function atualizarItensPedido() {
@@ -75,7 +69,6 @@ function atualizarItensPedido() {
         listaPedido.appendChild(li);
     });
 
-    
     let totalEl = document.getElementById("cart-total");
     if (!totalEl) {
         totalEl = document.createElement("p");
@@ -84,7 +77,6 @@ function atualizarItensPedido() {
     }
     totalEl.innerHTML = `<strong>Total:</strong> R$${total.toFixed(2)}`;
 }
-
 
 function atualizarItensCarrinho() {
     const itensCarrinho = document.getElementById("itensCarrinho");
@@ -96,8 +88,24 @@ function atualizarItensCarrinho() {
         itensCarrinho.appendChild(li);
     });
 
-    atualizarItensPedido(); // 
+    atualizarItensPedido(); 
 }
 
+
+function gerarQRCode(linkPagamento) {
+    const qrCodeContainer = document.getElementById('qrcode');
+    qrCodeContainer.innerHTML = ''; // Limpar qualquer QR Code anterior
+    QRCode.toCanvas(qrCodeContainer, linkPagamento, function (error) {
+        if (error) {
+            console.error(error);
+            alert("Erro ao gerar o QR Code.");
+        } else {
+            console.log('QR Code gerado com sucesso!');
+        }
+    });
+}
+
+
+const pagamentoLink = "00020126580014br.gov.bcb.pix0136a3fa6831-d2da-49ac-b31e-55bbcbe09e805204000053039865802BR5925MARIA EDUARDA GOMES ARRUD6009Sao Paulo62290525REC680EEEDEB552408665825263043C9D";
 
 document.getElementById("checkout-button").addEventListener("click", finalizarPedido);
